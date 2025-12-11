08571a8caae18a4916ed0d62ba8a8006d5b2bce7
soufiane <soufianeelbasraoui0@gmail.com>
2025-12-11 17:50:45 +0100

    update weather app

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,12 +1,13 @@
 
 
 const apiKey = "9b84f42d0f4625a201f4e06b12067f89";
-//const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=&units=metric&appid=${apiKey}`;
 
 const searchinput=document.getElementById("searchinput");
 const searchbtn=document.getElementById("searchbtn");
 
+
 async function checkWeather(city) {
+
     console.log(city)
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
     const data = await response.json();
@@ -18,23 +19,16 @@ async function checkWeather(city) {
     console.log(data.weather[0].icon)
     document.getElementById("img-city").src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
     convertTime(data);
-    conertersunrise(data)
-    ///////////////////////////////
+    convertsunrise(data)
      getForecast(data.coord.lat, data.coord.lon);
+     getWeekly(data.coord.lat,data.coord.lon)
 }
-//conertersunrise
-const conertersunrise=(data)=>{
-    const sunriseTimestamp=data.sys.sunrise;
-    const timezone = data.timezone;
-    const sunriseTime = new Date((sunriseTimestamp + timezone) * 1000);
-     const options = {
-    hour: '2-digit',
-    minute: '2-digit'
-     };
-     const formattedSunrise = sunriseTime.toLocaleTimeString('en-US', options);
-    document.getElementById("sunrise").innerHTML = formattedSunrise;
-}
-
+checkWeather("rabat");
+searchbtn.addEventListener("click", ()=>{
+    checkWeather(searchinput.value);
+      
+})
+//convertTime
 const convertTime=(data)=>{
         const teme=data.dt;
         const timezone=data.timezone;
@@ -50,15 +44,20 @@ const convertTime=(data)=>{
     const formatted = localTime.toLocaleString('en-US', options);
     document.getElementById("day").innerHTML=formatted;
     }
-checkWeather("rabat");
-
-searchbtn.addEventListener("click", ()=>{
-             checkWeather(searchinput.value);
-      
-})
+//conertersunrise
+const convertsunrise=(data)=>{
+    const sunriseTimestamp=data.sys.sunrise;
+    const timezone = data.timezone;
+    const sunriseTime = new Date((sunriseTimestamp + timezone) * 1000);
+     const options = {
+    hour: '2-digit',
+    minute: '2-digit'
+     };
+     const formattedSunrise = sunriseTime.toLocaleTimeString('en-US', options);
+    document.getElementById("sunrise").innerHTML = formattedSunrise;
+}
 // getForecast 
-async function getForecast(lat, lon) {
-   
+ function getForecast(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=9&appid=${apiKey}&units=metric`)
     .then(response => response.json())
     .then(data =>{ 
@@ -66,7 +65,6 @@ async function getForecast(lat, lon) {
     
     const forecastDiv = document.querySelector(".forecast");
     forecastDiv.innerHTML = ""; 
-
     data.list.forEach((item, index) => {
         const time = new Date(item.dt * 1000).toLocaleTimeString("en-US", {
             hour: "2-digit",
@@ -88,3 +86,33 @@ async function getForecast(lat, lon) {
     });
     })
 }
+//Weekly Forecast
+function getWeekly(lat,lon){
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+      .then(response => response.json())
+      .then(data =>{
+        console.log("weekly data : ", data);
+        const weekdayfascat=document.querySelector(".weekly-cards");
+        weekdayfascat.innerHTML="";
+         const dailyData = data.list.filter((item, index) => index % 8 === 0);
+        const sixdays=dailyData.slice(0, 6);
+        sixdays.forEach((item,index)=>{
+        
+        const dayName = new Date(item.dt * 1000).toLocaleDateString("en-US", { weekday: "long" });
+
+        const icons = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png`;
+        const temp = Math.round(item.main.temp);
+        const low = Math.round(item.main.temp_min);
+
+          weekdayfascat.innerHTML+=`
+          <div class="day-card">
+         <p class="day">${dayName}</p>
+            <img src="${icons}" width="35">
+            <p class="temp">${temp}°c</p>
+            <p class="low">${low} °c</p>
+         </div>
+         `
+        })
+       })
+}
+
